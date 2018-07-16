0d545beb0308a5c3f39bc13015ec0b5e85a32f3f
Thomas Moran <thmoran@deloitte.ie>
2018-07-16 12:33:37 +0100

    ready to deploy

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+  constructor() {
+    super();
+    this.state = {
+        name: ""
+    };
   }
+
+  
+    getUser = () => {
+        // Get the passwords and store them in state
+        fetch('/api/name')
+            .then(res => res.json())
+            .then(data => {
+                let name = data.name;
+                console.log(name);
+                this.setState({ "name": name })}
+            );
+            // .then(data => console.log(data.name));
+    }
+    
+    // Fetch passwords after first mount
+    componentDidMount() {
+        this.getUser();
+    }
+
+    render() {
+        let name = this.state.name;
+        
+        return (
+            <div className="App">
+                <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <h2>
+                    {name}
+                </h2>
+            </div>
+        );
+    }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
+app.get('/api/name', (req, res) => {
 
 	// Simulate data to return.
 	let userObj = {
